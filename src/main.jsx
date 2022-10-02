37f49ea853ab599d4314dc2fabd331d0cd2d50d7
12ahnaf34 <12ahnaf34@gmail.com>
2022-10-01 20:09:44 -0400

    added custom fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./fonts/bahnschrift.ttf";
+import "./fonts/norwester.otf";
 import Home from "./routes/Home";
 
 const router = createBrowserRouter([
--- src/routes/Home.styled.js
+++ src/routes/Home.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { colors } from "../GlobalStyles";
+import "../fonts/bahnschrift.ttf";
+import "../fonts/norwester.otf";
 
 export const MainContainer = styled.div`
   display: "grid";
@@ -8,6 +9,14 @@ export const MainContainer = styled.div`
   gap: 100px;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr 1fr 1fr;
+
+  @FontFace{
+    fontFamily:"Bahnschrift";
+    src: local("Bahnschrift"), url("../fonts/bahnschrift.ttf") format("truetype")
+  
+    fontFamily:"Norwester";
+    src: local("Norwester"), url("../fonts/norwester.otf") format("opentype")
+  }
 `;
 
 export const ScrollbarWrapper = styled.div`
