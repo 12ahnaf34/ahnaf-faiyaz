68650d7b85ca7e14f89a0940a015f0d53c24f11f
12ahnaf34 <12ahnaf34@gmail.com>
2022-10-02 08:34:09 -0400

    font fix minor

--- src/fonts/GlobalFonts.js
+++ src/fonts/GlobalFonts.js
@@ -1,17 +1,16 @@
 import { createGlobalStyle } from "styled-components";
-
-import Bahnschrift from "./Bahnschrift.ttf";
+import Bahnschrift from "./Bahnschrift.woff2";
 import Norwester from "./Norwester.woff2";
 
 const GlobalFonts = createGlobalStyle`
-    @font-face {
-        font-family: 'Bahnschrift';
-        src: url(${Bahnschrift}) format('truetype');
-    }
+  @font-face {
+    font-family: 'Bahnschrift';
+    src: url(${Bahnschrift}) format('woff2');
+  }    
 
-    @font-face {
-      font-family: 'Norwester';
-      src: url(${Norwester}) format('woff2');
+  @font-face {
+        font-family: 'Norwester';
+        src: url(${Norwester}) format('woff2');
   }
 `;
 
