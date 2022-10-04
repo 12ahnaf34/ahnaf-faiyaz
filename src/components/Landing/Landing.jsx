af6c6f038bdbe4e14da6257d17494951e014c309
12ahnaf34 <12ahnaf34@gmail.com>
2022-10-03 22:07:41 -0400

    added function to landing page button

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,12 +3,14 @@ import { Button } from "../../GlobalStyles";
 import { LandingContainer, MinorContainer, Paragraph, Image, ProjectsButton } from "./Landing.styled";
 
 export default function Landing(props) {
+  const { goTo, portfolioProjectsSection } = props;
+
   return (
     <LandingContainer>
       <Image src="/logo1.png" />
       <MinorContainer>
         <Paragraph>Frontend Developer</Paragraph>
-        <ProjectsButton primary bigFont>
+        <ProjectsButton primary bigFont onClick={() => goTo(portfolioProjectsSection)}>
           Projects
         </ProjectsButton>
       </MinorContainer>
